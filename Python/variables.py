abe201d5694b8fb57ea161e6dead6928ae833b65
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-01 20:46:13 +0700

    Create variables.py

---
+++ Python/variables.py
@@ -0,0 +1,33 @@
+# **MỘT SỐ VÍ DỤ VỀ COMMENT TRONG PYTHON**
+
+```python
+#vi du thu 1
+```
+
+```python
+#in ra hello
+print("hello")
+```
+
+Bạn cũng có thể vô hiệu hóa, một dòng code bằng cách comment!
+
+```python
+#print("Toi khong in ra hello world nua")
+#co the comment nhieu dong nhu nay
+```
+
+Bạn cũng có thể comment nhiều dòng như này chẳng hạn
+
+```python
+"""
+KHI BAN NHAP VAO A
+VA NHAP VAO B
+THI TOI SE TRA CHO BAN KET QUA C LA A + B
+"""
+a = int(input("A="))
+b = int(input("B="))
+c = a + b
+print("C="+c)
+```
+
+**Cách thức hoạt động của comment**: Những dòng code được comment, khi biên dịch chương trình sẽ bỏ qua và không dịch nó.
