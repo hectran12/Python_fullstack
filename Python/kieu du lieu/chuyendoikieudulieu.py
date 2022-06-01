d8afa02846be6f773bba843facea8b453c920fc0
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-01 20:50:00 +0700

    Create chuyendoikieudulieu.py

---
+++ Python/kieu du lieu/chuyendoikieudulieu.py
@@ -0,0 +1,30 @@
+#Chuyển đổi sang kiểu dữ liệu chuỗi (string)
+myString = str("Xin chào")
+print(myString)
+print(type(myString))
+
+#Chuyển đổi sang kiểu dữ liệu số (integer)
+myNumberic = int(8)
+print(myNumberic)
+print(type(myNumberic))
+
+#Chuyển đổi sang kiểu dữ liệu float
+myFloatNumber = float(3)
+myFloatNumberTwo = float(3.5)
+print(myFloatNumber)
+print(myFloatNumberTwo)
+print(type(myFloatNumber))
+print(type(myFloatNumberTwo))
+
+#Chuyển đổi từ tuple sang list
+myTuple = ("Táo","Vàng","Xanh")
+print(myTuple)
+print(type(myTuple))
+myListConver = list(myTuple)
+print(myListConver)
+print(type(myListConver))
+
+#Chuyển name=value sang dict (json)
+myDict = dict(myName="Trong Hoa",myAge=13)
+print(myDict)
+print(type(myDict))
