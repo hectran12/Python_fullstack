080bdeb5fa57b61d15275fd21f72ec1f5022660e
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-02 13:02:40 +0700

    Create convert.py

---
+++ Python/kieu du lieu/integer/convert.py
@@ -0,0 +1,12 @@
+numberA = 1 
+numberB = 1.4 
+numberC = 1j
+
+#convert
+resA = float(numberA) # chuyển numberA từ int sang float
+resB = int(numberB) # chuyển numberB từ float sang int
+#resC = float(numberC) # Đối với complex thì không thể chuyển đổi
+
+#output
+print(type(resA))
+print(type(resB))
