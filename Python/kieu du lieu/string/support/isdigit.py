695c0a25f2cee647c4b7cd06257d4d2b0cf92339
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-10 10:33:42 +0700

    Create isdigit.py

---
+++ Python/kieu du lieu/string/support/isdigit.py
@@ -0,0 +1,5 @@
+# isdigit() - Kiểm tra một chuỗi có phải toàn số hay không
+myNumber = "100"
+print(myNumber.isdigit()) # True - vì trong chuỗi là số 100
+myNumber = "1o"
+print(myNumber.isdigit()) # False - vì trong chuỗi là 1o , và o là kí tự
