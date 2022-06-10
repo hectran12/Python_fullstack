ebca342e8b82f79416b35b0732a44cce67180c87
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-10 10:27:24 +0700

    Create encode.py

---
+++ Python/kieu du lieu/string/support/encode.py
@@ -0,0 +1,4 @@
+# encode() - Encode string
+myText = "Trọng Hòa đẹp trai"
+Encode_normal = myText.encode() # Encode sang mã utf-8
+print(Encode_normal); # output: b'Tr\xe1\xbb\x8dng H\xc3\xb2a \xc4\x91\xe1\xba\xb9p trai'
