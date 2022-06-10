350c53c40fd94e7a4e9b59cea6a5c16c2071ecbe
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-10 10:23:26 +0700

    Create not_in.py

---
+++ Python/kieu du lieu/string/support/not_in.py
@@ -0,0 +1,5 @@
+myString = "Xin chao"
+checkNotIn = "Xin" not in myString
+print(checkNotIn) # False - Có trong chuỗi
+checkNotIn = "Hoa" not in myString
+print(checkNotIn) # True - không có trong chuỗi
